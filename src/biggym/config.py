a89d2470bc4619e38172e368bd36f2bc4cab102b
jamesrj_desktop <james.rudd.jones@gmail.com>
2024-06-04 17:39:14 +0100

    initial commit with tabular q

---
+++ src/biggym/config.py
@@ -0,0 +1,21 @@
+from ml_collections import config_dict
+
+
+def get_config():
+    config = config_dict.ConfigDict()
+    config.SEED = 42
+    config.LR = 0.08
+    config.GAMMA = 0.95
+    config.EPS = 1
+    config.NUM_EPISODES = 5000  # 100000
+
+    config.EPS_DECAY = 0.8
+
+    # config.WANDB = "disabled"  # "online" if want it to work
+    config.WANDB = "online"
+
+    config.WANDB_ENTITY = "jamesr-j"  # change this to your wandb username
+
+    config.PLOT = True
+
+    return config
