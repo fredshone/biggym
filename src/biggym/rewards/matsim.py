7bd2bf4e49d3f9c0d3bae8ef54bc6423bed40a70
Fred Shone <26383933+fredshone@users.noreply.github.com>
2024-06-26 15:24:06 +0100

    adds trace expectation

--- src/biggym/rewards/matsim.py
+++ src/biggym/rewards/matsim.py
@@ -1,5 +1,6 @@
-import numpy as np
 from typing import Optional
+
+import numpy as np
 
 
 class SimpleMATSimTraceScorer:
@@ -75,32 +76,8 @@ class SimpleMATSimTraceScorer:
                 },
             },
         }
-
-    def min(self, distance: Optional[float] = None):
-        # assume 24 hour period
-        # assume zero benefit from activities
-        # assume 2 trips of 12 hours each
-        mum = self.config.get("mUM", 1)
-        if distance is None:
-            distance = 100
-        costs = []
-        # find most expensive mode
-        for _, params in self.config["modes"].items():
-            cost = 0
-            cost += params.get("constant", 0)
-            cost += params.get("dailyMonetaryConstant", 0) * mum
-            cost += params.get("dailyUtilityConstant", 0)
-            cost += params.get("marginalUtilityOfDistance", 0) * distance
-            cost += params.get("marginalUtilityOfTravelling", 0) * 12
-            cost += params.get("monetaryDistanceRate", 0) * distance * mum
-            costs.append(cost)
-        return min(costs) * 2
-
-    def max(self):
-        # assume 24 hour period
-        # assume zero cost from trips
-        # assume max value of performing achieved without penalty
-        return self.config["performing"] * 24
+        self.min = self.min()
+        self.max = self.max()
 
     def score(self, trace: list, obs_map: dict):
         score = 0.0
@@ -202,6 +179,32 @@ class SimpleMATSimTraceScorer:
             if end < earliest_end_time:
                 return self.config["earlyDeparture"] * (earliest_end_time - end)
         return 0.0
+
+    def min(self, distance: Optional[float] = None):
+        # assume 24 hour period
+        # assume zero benefit from activities
+        # assume 2 trips of 12 hours each
+        mum = self.config.get("mUM", 1)
+        if distance is None:
+            distance = 100
+        costs = []
+        # find most expensive mode
+        for _, params in self.config["modes"].items():
+            cost = 0
+            cost += params.get("constant", 0)
+            cost += params.get("dailyMonetaryConstant", 0) * mum
+            cost += params.get("dailyUtilityConstant", 0)
+            cost += params.get("marginalUtilityOfDistance", 0) * distance
+            cost += params.get("marginalUtilityOfTravelling", 0) * 12
+            cost += params.get("monetaryDistanceRate", 0) * distance * mum
+            costs.append(cost)
+        return min(costs) * 2
+
+    def max(self):
+        # assume 24 hour period
+        # assume zero cost from trips
+        # assume max value of performing achieved without penalty
+        return self.config["performing"] * 24
 
 
 def wrap_trace(trace):
